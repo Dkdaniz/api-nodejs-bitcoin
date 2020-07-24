5d9cb3b38f87a136f2fe1987cde2ca80bb308608
dkdaniz <daniel@descentralizado.com.br>
2020-07-24 02:11:31 -0300

    [ADD] config testnet bitcoin

--- src/config/bitcoin.ts
+++ src/config/bitcoin.ts
@@ -5,3 +5,8 @@ const endpoint = `159.89.53.60:${rpcport}/`;
 const rpcHref = `http://${rpcuser}:${rpcpassword}@${endpoint}`;
 
 export default { endpoint: rpcHref };
+
+async function balance() {
+  const balance = await client.getBalance('*', 0);
+  console.log(balance);
+}
